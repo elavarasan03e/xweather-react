46508dbfc48284917a4f4431d9f00797ce7bb2e0
elavarasan03e <101381035+elavarasan03e@users.noreply.github.com>
2024-05-05 17:07:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -38,12 +38,19 @@ function App() {
       {loading && <p>Loading data...</p>}
       <div className="weather-cards">
         {weatherData && (
-          <div className="weather-card">
-            <p>Temperature {weatherData.current.temp_c}°C</p>
-            <p>Humidity {weatherData.current.humidity}%</p>
-            <p>Condition {weatherData.current.condition.text}</p>
-            <p>Wind Speed {weatherData.current.wind_kph} kph</p>
-          </div>
+          <><div className="weather-card">
+            <h2>Temperature</h2>
+            <p>{weatherData.current.temp_c}°C</p>
+          </div><div className="weather-card">
+              <h2>Humidity</h2>
+              <p>{weatherData.current.humidity}%</p>
+            </div><div className="weather-card">
+              <h2>Condition</h2>
+              <p>{weatherData.current.condition.text}</p>
+            </div><div className="weather-card">
+              <h2>Wind Speed</h2>
+              <p>{weatherData.current.wind_kph} km/h</p>
+            </div></>
         )}
       </div>
     </div>
